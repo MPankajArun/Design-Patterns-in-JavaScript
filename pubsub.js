a003f01910cf3f6a5080c28c053b5ae7aaf9c789
Pankaj Meshram <pankaj_meshram@persistent.co.in>
2018-01-05 17:17:12 +0530

    First commit: Added a local repository to Github

---
+++ pubsub.js
@@ -0,0 +1,61 @@
+;(function ( window, doc, undef ) {
+    'use strict';
+ 
+     var topics = {},
+         subUid = -1,
+         pubsubz ={};
+ 
+     pubsubz.publish = function ( topic, args ) {
+ 
+         if (!topics[topic]) {
+             return false;
+         }
+ 
+         setTimeout(function () {
+             var subscribers = topics[topic],
+                 len = subscribers ? subscribers.length : 0;
+ 
+             while (len--) {
+                 subscribers[len].func(topic, args);
+             }
+         }, 0);
+ 
+         return true;
+ 
+     };
+ 
+     pubsubz.subscribe = function ( topic, func ) {
+ 
+         if (!topics[topic]) {
+             topics[topic] = [];
+         }
+ 
+         var token = (++subUid).toString();
+         topics[topic].push({
+             token: token,
+             func: func
+         });
+         return token;
+     };
+ 
+     pubsubz.unsubscribe = function ( token ) {
+         for (var m in topics) {
+             if (topics[m]) {
+                 for (var i = 0, j = topics[m].length; i < j; i++) {
+                     if (topics[m][i].token === token) {
+                         topics[m].splice(i, 1);
+                         return token;
+                     }
+                 }
+             }
+         }
+         return false;
+     };
+ 
+     var getPubSubz = function(){
+         return pubsubz;
+     };
+ 
+     window.pubsubz = getPubSubz();
+ 
+ }( this, this.document ));
